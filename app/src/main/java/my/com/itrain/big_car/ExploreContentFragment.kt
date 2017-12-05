6acbf7e93891118a4e00a6b0b9f671d928ef96db
KanEsh314 <kanesh314@gmail.com>
2017-12-05 21:49:04 +0800

    Explore Fragment

--- app/src/main/java/my/com/itrain/big_car/ExploreContentFragment.kt
+++ app/src/main/java/my/com/itrain/big_car/ExploreContentFragment.kt
@@ -1,11 +1,14 @@
 package my.com.itrain.big_car
 
 
+import `in`.goodiebag.carouselpicker.CarouselPicker
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_explore_content.*
+import java.util.ArrayList
 
 
 /**
@@ -15,8 +18,11 @@ class ExploreContentFragment : Fragment() {
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_saved_content, container, false)
+        var rootView = inflater.inflate(R.layout.fragment_explore_content, container, false)
+
+        return rootView
     }
 
 }// Required empty public constructor
---
+++ app/src/main/java/my/com/itrain/big_car/SplashActivity.kt
@@ -0,0 +1,21 @@
+package my.com.itrain.big_car
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : Activity() {
+
+    private val splash_screen = 2000;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        },splash_screen.toLong())
+    }
+}
