fcc47004ded651ba32a5959141f17251b2db1d68
KanEsh314 <kanesh314@gmail.com>
2018-04-12 19:09:40 +0800

    Insert Image

--- app/src/main/java/my/com/itrain/big_car/BrowseContentFragment.kt
+++ app/src/main/java/my/com/itrain/big_car/BrowseContentFragment.kt
@@ -1,43 +1,12 @@
 package my.com.itrain.big_car
 
-
-import android.app.VoiceInteractor
-import android.content.Context
-import android.content.Intent
-import android.graphics.Rect
-import android.graphics.drawable.Drawable
 import android.os.Bundle
-import android.support.annotation.DimenRes
-import android.support.annotation.NonNull
 import android.support.v4.app.Fragment
-import android.support.v7.widget.DefaultItemAnimator
-import android.support.v7.widget.GridLayoutManager
-import android.support.v7.widget.RecyclerView
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
-import com.android.volley.Request
-import com.android.volley.Response
-import com.android.volley.VolleyError
-import com.android.volley.toolbox.JsonArrayRequest
-import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.Volley
-import com.google.android.gms.common.api.Status
-import com.google.android.gms.location.places.Place
-import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment
-import com.google.android.gms.location.places.ui.PlaceSelectionListener
-import com.google.android.gms.location.places.ui.SupportPlaceAutocompleteFragment
 import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
-import kotlinx.android.synthetic.main.fragment_browse_content.*
-import org.json.JSONArray
-import org.json.JSONException
-import org.json.JSONObject
-import java.time.DayOfWeek
-
 
 /**
  * A simple [Fragment] subclass.
